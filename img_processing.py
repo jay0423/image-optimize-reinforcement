d7271ab042af868e1e0510759089fd9e1e1e4f87
jay0423 <jay0423@i.softbank.jp>
2021-06-16 19:14:35 +0900

    get_mask_bgrに関する細かい修正

--- img_processing.py
+++ img_processing.py
@@ -45,7 +45,7 @@ class ImgProcessing:
 
 
     # 赤だけ残した画像を出力
-    def get_mask_bgr(self, bgr_img, get_color="g", m_min=200, m_max=255, frame=True, filtering=True):
+    def get_mask_bgr(self, bgr_img, get_color="r", m_min=200, m_max=255, frame=True, filtering=True):
         
         # 3色に分離（チャンネルを分ける）
         b, g, r = cv2.split(bgr_img)
@@ -57,17 +57,14 @@ class ImgProcessing:
                 color_list[i] = ""
         except_color_bgr_list = [b, g, r]
         except_color_list = [e for e, c in zip(except_color_bgr_list, color_list) if c != ""]
-        # for e, c in zip(except_color_bgr_list, color_list):
-        #     if c != "":
-        #         except_color_list.append(e)
 
         # 指定されたカラーをマスクする処理
         except_img =bgr_img.copy()
         for color in except_color_list:
-            _, mask_img = cv2.threshold(color, 200, 255, cv2.THRESH_BINARY_INV) # マスク（黒にする）
+            _, mask_img = cv2.threshold(color, m_min, m_max, cv2.THRESH_BINARY_INV) # マスク（黒にする）
             except_img = cv2.bitwise_and(except_img, except_img, mask=mask_img) # 除外
 
-        #　外枠の取得と合成
+        #　初期画像の白部分の取得と合成
         if frame:
             # グレースケール化
             gry = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY)
@@ -87,7 +84,7 @@ class ImgProcessing:
     def processing(self, img_list):
         img_processed_list = [] #画像処理が完了した画像リスト
         for img in img_list:
-            img_processed_list.append(self.get_red(img)) #赤色を摘出し格納
+            img_processed_list.append(self.get_mask_bgr(img, get_color="r", m_min=210, frame=False)) #赤色を摘出し格納
         return img_processed_list
     
 
@@ -100,32 +97,6 @@ class ImgProcessing:
         self.img_cleaned_list = self.cleaning(self.img_list)
         self.img_processed_list = self.processing(self.img_cleaned_list)
         print("画像を合成させました．")
-
-
-
-    # def show_img(self, img, vmin=0, vmax=255, title=None):
-    #     if img.ndim == 3:
-    #         #カラー(ndim=次元数=3 -> RGBの３色)
-    #         rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #CV2のRGB３色に変換
-    #         plt.imshow(rgb_img, vmin=vmin, vmax=vmax) #変換後画像をplotへ設定
-    #     elif img.ndim == 2:
-    #         #グレースケール（ndim=次元数=2 -> 白黒の２色）
-    #         plt.imshow(img, cmap='gray', vmin=vmin, vmax=vmax) #変換後画像をplotへ設定
-    #     plt.axis('off') #座標軸非表示
-    #     plt.title(title) #タイトル設定
-    #     # plt.show() #グラフ表示
-
-        
-    #画像表示（比較）
-    def show_img_compare(self, src, dst, vmin=0, vmax=255):
-        # 元画像表示
-        plt.subplot(121)
-        self.show_img(src, vmin, vmax, title='Before')
-            
-        # 返還後画像表示
-        plt.subplot(122)
-        self.show_img(dst, vmin, vmax, title='After')
-
 
 
     #複数画像を同時に表示する．
